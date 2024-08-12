fa2b138d89fbb00368e2aa002410ec679e749843
nepalhomeland <ashokabrother@gmail.com>
2024-08-12 06:54:47 +0545

    Header logo description change

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -7,12 +7,12 @@ function Intro() {
       <div className='flex justify-center pt-8 gap-y-10'>
         <div className='h-[400px] w-[400px] bg-black rounded-full flex justify-center items-center  '>
 
-          <div className='text-white p-10 text-center mx-4 space-y-6'>
+          <div className='text-white text-xl p-10 text-center mx-4 space-y-2'>
             
             <p>Name : Ashok Neupane</p>
-            <p>Experience: HTML, CSS, JavaScript, React, NodeJs Express, MongoDB</p>
+            <p>Experience: <br /> HTML, <br />CSS,<br /> JavaScript, <br /> React, NodeJs, Express, MongoDB</p>
             <p>Email: ashokabrother@gmail.com</p>
-            <p>Tel:+977987656</p>
+            <p>Tel:+97798765643</p>
           </div>
         </div>
       </div>
